b158fd67f0cef11e39ba0b418fd3d9c2ec43fb96
vaibhavxom <rmithagare02@gmail.com>
2025-06-10 00:51:55 +0530

    deployment commit

--- src/main/resources/portfolio-frontend/next.config.mjs
+++ src/main/resources/portfolio-frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/dev-portfolio',        // Replace with your repo name
+  assetPrefix: '/dev-portfolio/',    // Replace with your repo name
+};
 
 export default nextConfig;
