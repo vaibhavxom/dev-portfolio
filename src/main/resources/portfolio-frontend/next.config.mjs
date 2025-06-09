a98e1d03be13b199bf9e80abc2759c38066eb119
vaibhavxom <rmithagare02@gmail.com>
2025-06-10 01:05:33 +0530

    deployment script updated

--- src/main/resources/portfolio-frontend/next.config.mjs
+++ src/main/resources/portfolio-frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/dev-portfolio',        // Replace with your repo name
-  assetPrefix: '/dev-portfolio/',    // Replace with your repo name
+  basePath: isProd ? '/dev-portfolio' : '',
+  assetPrefix: isProd ? '/dev-portfolio/' : '',
 };
 
 export default nextConfig;
